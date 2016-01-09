058cc16b869e0de8d06072210dcf3a1509220d01
Michael Karbowiak <karbowiak@gmail.com>
2016-01-09 14:09:12 +0100

    Move API controllers to their own, since they're gonna become huge and bloated real fast

--- config/routes/api.php
+++ config/routes/api.php
@@ -4,32 +4,32 @@ $app->group("/api", function () use ($app) {
     // Data for a character
     $app->group("/character", function () use ($app) {
         $app->get("/information/:characterID/", function ($characterID) use ($app) {
-            (new \ProjectRena\Controller\APIController($app))->characterInformation($characterID);
+            (new \ProjectRena\Controller\API\CharacterAPIController($app))->characterInformation($characterID);
         });
     });
 
     // Data for a corporation
     $app->group("/corporation", function () use ($app) {
         $app->get("/information/:corporationID/", function ($corporationID) use ($app) {
-            (new \ProjectRena\Controller\APIController($app))->corporationInformation($corporationID);
+            (new \ProjectRena\Controller\API\CorporationAPIController($app))->corporationInformation($corporationID);
         });
         $app->get("/members/:corporationID/", function ($corporationID) use ($app) {
-            (new \ProjectRena\Controller\APIController($app))->corporationMembers($corporationID);
+            (new \ProjectRena\Controller\API\CorporationAPIController($app))->corporationMembers($corporationID);
         });
     });
 
     // Data for an alliance
     $app->group("/alliance", function () use ($app) {
         $app->get("/information/:allianceID/", function ($allianceID) use ($app) {
-            (new \ProjectRena\Controller\APIController($app))->allianceInformation($allianceID);
+            (new \ProjectRena\Controller\API\AllianceAPIController($app))->allianceInformation($allianceID);
         });
         $app->get("/members/:allianceID/", function ($allianceID) use ($app) {
-            (new \ProjectRena\Controller\APIController($app))->allianceMembers($allianceID);
+            (new \ProjectRena\Controller\API\AllianceAPIController($app))->allianceMembers($allianceID);
         });
     });
 
     // Killmail
     $app->get("/killmail/:killID/", function($killID) use ($app) {
-        (new \ProjectRena\Controller\APIController($app))->killData($killID);
+        (new \ProjectRena\Controller\API\KillmailsAPIController($app))->killData($killID);
     });
 });
