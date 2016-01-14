f1bddf20853badf1bbafa3323e8681fce81bda79
Michael Karbowiak <karbowiak@gmail.com>
2016-01-14 23:45:10 +0100

    Add insert members by id and name, and insert coalition to the coalitions model

--- src/Model/Database/EVE/coalitions.php
+++ src/Model/Database/EVE/coalitions.php
@@ -110,4 +110,39 @@ class coalitions
     {
         return $this->app->Db->queryField("SELECT coalitionMembers FROM coalitions WHERE coalitionName = :coalitionName", "coalitionMembers", array(":coalitionName" => $coalitionName));
     }
+
+    /**
+     * @param $coalitionID
+     * @param array $member
+     */
+    public function insertMemberToCoalitionByID($coalitionID, $member = array("memberID" => 0, "memberType" => "characterID"))
+    {
+        $data = json_decode($this->getMembersByID($coalitionID), true);
+        $data[] = $member;
+
+        $this->app->Db->execute("UPDATE coalitions SET coalitionMembers = :coalitionMembers WHERE coalitionID = :coalitionID", array(":coalitionID" => $coalitionID, ":coalitionMembers" => json_encode($data)));
+    }
+
+    /**
+     * @param $coalitionName
+     * @param array $member
+     */
+    public function insertMemberToCoalitionByName($coalitionName, $member = array("memberID" => 0, "memberType" => "characterID"))
+    {
+        $data = json_decode($this->getMembersByID($coalitionName), true);
+        $data[] = $member;
+
+        $this->app->Db->execute("UPDATE coalitions SET coalitionMembers = :coalitionMembers WHERE coalitionName = :coalitionName", array(":coalitionName" => $coalitionName, ":coalitionMembers" => json_encode($data)));
+    }
+
+    /**
+     * @param $coalitionID
+     * @param $coalitionName
+     * @param array $coalitionMembers
+     */
+    public function insertCoalition($coalitionID, $coalitionName, $coalitionMembers = array())
+    {
+        $coalitionMembers = json_encode($coalitionMembers);
+        $this->app->Db->execute("INSERT INTO coalitions (coalitionID, coalitionName, coalitionMembers) VALUES (:coalitionID, :coalitionName, :coalitionMembers)", array(":coalitionID" => $coalitionID, ":coalitionName" => $coalitionName, ":coalitionMembers" => $coalitionMembers));
+    }
 }
