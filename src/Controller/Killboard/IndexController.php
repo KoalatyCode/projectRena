3ce176ea6ccf500d3e45329279e28777f28a5efc
Michael Karbowiak <karbowiak@gmail.com>
2016-02-11 00:15:31 +0100

    Move DbAsync around a bit, and add the cache properly.. it's still not pretty but for now it'll work just fine

--- src/Controller/Killboard/IndexController.php
+++ src/Controller/Killboard/IndexController.php
@@ -80,38 +80,38 @@ class IndexController
         $data["killListData"] = $this->app->killmails->getKill_jsonByKillIDs($killIDs);
 
         // Get the current count of active chars/corps/alliance/ships/systems
-        $db->currAcChar("SELECT COUNT(DISTINCT(characterID)) AS count FROM participants WHERE killTime >= DATE_SUB(now(), interval 7 day)");
-        $db->currAcCorp("SELECT COUNT(DISTINCT(corporationID)) AS count FROM participants WHERE killTime >= DATE_SUB(now(), interval 7 day)");
-        $db->currAcAlli("SELECT COUNT(DISTINCT(allianceID)) AS count FROM participants WHERE killTime >= DATE_SUB(now(), interval 7 day)");
-        $db->currAcStyp("SELECT COUNT(DISTINCT(shipTypeID)) AS count FROM participants WHERE killTime >= DATE_SUB(now(), interval 7 day)");
-        $db->currAcSols("SELECT COUNT(DISTINCT(solarSystemID)) AS count FROM participants WHERE killTime >= DATE_SUB(now(), interval 7 day)");
+        $db->executeQuery("currentActiveCharacters", "SELECT COUNT(DISTINCT(characterID)) AS count FROM participants WHERE killTime >= DATE_SUB(now(), interval 7 day)", 3600);
+        $db->executeQuery("currentActiveCorporations", "SELECT COUNT(DISTINCT(corporationID)) AS count FROM participants WHERE killTime >= DATE_SUB(now(), interval 7 day)", 3600);
+        $db->executeQuery("currentActiveAlliances", "SELECT COUNT(DISTINCT(allianceID)) AS count FROM participants WHERE killTime >= DATE_SUB(now(), interval 7 day)", 3600);
+        $db->executeQuery("currentActiveShipTypes", "SELECT COUNT(DISTINCT(shipTypeID)) AS count FROM participants WHERE killTime >= DATE_SUB(now(), interval 7 day)", 3600);
+        $db->executeQuery("currentActiveSolarSystems", "SELECT COUNT(DISTINCT(solarSystemID)) AS count FROM participants WHERE killTime >= DATE_SUB(now(), interval 7 day)", 3600);
 
         // Get the current amount of kills done over the last 7 days
-        $db->killCountOverLast7Days("SELECT COUNT(*) AS count FROM participants WHERE killTime >= DATE_SUB(now(), interval 7 day) AND isVictim = 1");
+        $db->executeQuery("killCountOverLast7Days", "SELECT COUNT(*) AS count FROM participants WHERE killTime >= DATE_SUB(now(), interval 7 day) AND isVictim = 1", 3600);
 
         // Get the top 10 characters/corporations/alliances/ships/systems/regions for the last 7 days
-        $db->top10Characters("SELECT characterID, COUNT(DISTINCT(killID)) AS count FROM participants WHERE killTime >= DATE_SUB(now(), interval 7 day) AND characterID != 0 GROUP BY characterID ORDER BY count DESC LIMIT 10");
-        $db->top10Corporations("SELECT corporationID, COUNT(DISTINCT(killID)) AS count FROM participants WHERE killTime >= DATE_SUB(now(), interval 7 day) AND corporationID != 0 GROUP BY corporationID ORDER BY count DESC LIMIT 10");
-        $db->top10Alliances("SELECT allianceID, COUNT(DISTINCT(killID)) AS count FROM participants WHERE killTime >= DATE_SUB(now(), interval 7 day) AND allianceID != 0 GROUP BY allianceID ORDER BY count DESC LIMIT 10");
-        $db->top10shipTypes("SELECT shipTypeID, COUNT(DISTINCT(killID)) AS count FROM participants WHERE killTime >= DATE_SUB(now(), interval 7 day) AND shipTypeID != 0 GROUP BY shipTypeID ORDER BY count DESC LIMIT 10");
-        $db->top10SolarSystems("SELECT solarSystemID, COUNT(DISTINCT(killID)) AS count FROM participants WHERE killTime >= DATE_SUB(now(), interval 7 day) AND solarSystemID != 0 GROUP BY solarSystemID ORDER BY count DESC LIMIT 10");
-        $db->top10Regions("SELECT regionID, COUNT(DISTINCT(killID)) AS count FROM participants WHERE killTime >= DATE_SUB(now(), interval 7 day) AND regionID != 0 GROUP BY regionID ORDER BY count DESC LIMIT 10");
+        $db->executeQuery("top10Characters", "SELECT characterID, COUNT(DISTINCT(killID)) AS count FROM participants WHERE killTime >= DATE_SUB(now(), interval 7 day) AND characterID != 0 GROUP BY characterID ORDER BY count DESC LIMIT 10", 3600);
+        $db->executeQuery("top10Corporations", "SELECT corporationID, COUNT(DISTINCT(killID)) AS count FROM participants WHERE killTime >= DATE_SUB(now(), interval 7 day) AND corporationID != 0 GROUP BY corporationID ORDER BY count DESC LIMIT 10", 3600);
+        $db->executeQuery("top10Alliances", "SELECT allianceID, COUNT(DISTINCT(killID)) AS count FROM participants WHERE killTime >= DATE_SUB(now(), interval 7 day) AND allianceID != 0 GROUP BY allianceID ORDER BY count DESC LIMIT 10", 3600);
+        $db->executeQuery("top10ShipTypes", "SELECT shipTypeID, COUNT(DISTINCT(killID)) AS count FROM participants WHERE killTime >= DATE_SUB(now(), interval 7 day) AND shipTypeID != 0 GROUP BY shipTypeID ORDER BY count DESC LIMIT 10", 3600);
+        $db->executeQuery("top10SolarSystems", "SELECT solarSystemID, COUNT(DISTINCT(killID)) AS count FROM participants WHERE killTime >= DATE_SUB(now(), interval 7 day) AND solarSystemID != 0 GROUP BY solarSystemID ORDER BY count DESC LIMIT 10", 3600);
+        $db->executeQuery("top10Regions", "SELECT regionID, COUNT(DISTINCT(killID)) AS count FROM participants WHERE killTime >= DATE_SUB(now(), interval 7 day) AND regionID != 0 GROUP BY regionID ORDER BY count DESC LIMIT 10", 3600);
 
         // Get the most valuable kills for the last 7 days
 
 
-        $data["currentActive"]["characters"] = $db->currAcChar()->fetch_assoc();
-        $data["currentActive"]["corporations"] = $db->currAcCorp()->fetch_assoc();
-        $data["currentActive"]["alliances"] = $db->currAcAlli()->fetch_assoc();
-        $data["currentActive"]["shipTypes"] = $db->currAcStyp()->fetch_assoc();
-        $data["currentActive"]["solarSystems"] = $db->currAcSols()->fetch_assoc();
-        $data["killCountOverLast7Days"] = $db->killCountOverLast7Days()->fetch_assoc();
-        $data["top10"]["characters"] = $db->top10Characters()->fetch_assoc();
-        $data["top10"]["corporations"] = $db->top10Corporations()->fetch_assoc();
-        $data["top10"]["alliances"] = $db->top10Alliances()->fetch_assoc();
-        $data["top10"]["shipTypes"] = $db->top10shipTypes()->fetch_assoc();
-        $data["top10"]["solarSystems"] = $db->top10SolarSystems()->fetch_assoc();
-        $data["top10"]["regions"] = $db->top10Regions()->fetch_assoc();
+        $data["currentActive"]["characters"] = $db->getData("currentActiveCharacters", 3600)["count"];
+        $data["currentActive"]["corporations"] = $db->getData("currentActiveCorporations", 3600)["count"];
+        $data["currentActive"]["alliances"] = $db->getData("currentActiveAlliances", 3600)["count"];
+        $data["currentActive"]["shipTypes"] = $db->getData("currentActiveShipTypes", 3600)["count"];
+        $data["currentActive"]["solarSystems"] = $db->getData("currentActiveSolarSystems", 3600)["count"];
+        $data["killCountOverLast7Days"] = $db->getData("killCountOverLast7Days", 3600)["count"];
+        $data["top10"]["characters"] = $db->getData("top10Characters", 3600);
+        $data["top10"]["corporations"] = $db->getData("top10Corporations", 3600);
+        $data["top10"]["alliances"] = $db->getData("top10Alliances", 3600);
+        $data["top10"]["shipTypes"] = $db->getData("top10ShipTypes", 3600);
+        $data["top10"]["solarSystems"] = $db->getData("top10SolarSystems", 3600);
+        $data["top10"]["regions"] = $db->getData("top10Regions", 3600);
 
         render("", $data, null, "application/json");
     }
--- src/Lib/DbAsync.php
+++ src/Lib/DbAsync.php
@@ -24,20 +24,36 @@ class DbAsync
             $connection->close();
     }
 
-    function __call($name, array $arguments)
+    public function executeQuery($name, $query, $cacheTime = 3600)
     {
-        if($arguments) {
-            $host = $this->app->baseConfig->getConfig("host", "database", "127.0.0.1");
-            $username = $this->app->baseConfig->getConfig('username', 'database');
-            $password = $this->app->baseConfig->getConfig('password', 'database');
-            $dbName = $this->app->baseConfig->getConfig('name', 'database');
-            $port = $this->app->baseConfig->getConfig('port', 'database', 3306);
-            $socket = $this->app->baseConfig->getConfig("unixSocket", "database", "/var/run/mysqld/mysqld.sock");
+        $key = sha1($name);
 
-            $query = $arguments[0];
-            $connection = mysqli_connect($host, $username, $password, $dbName, $port, $socket);
-            $this->connections[$name] = $connection;
-            return $connection->query($query, MYSQLI_ASYNC);
+        if($cacheTime > 0) {
+            if(!empty($this->app->Cache->get($key)))
+                return;
+        }
+
+        $host = $this->app->baseConfig->getConfig("host", "database", "127.0.0.1");
+        $username = $this->app->baseConfig->getConfig('username', 'database');
+        $password = $this->app->baseConfig->getConfig('password', 'database');
+        $dbName = $this->app->baseConfig->getConfig('name', 'database');
+        $port = $this->app->baseConfig->getConfig('port', 'database', 3306);
+        $socket = $this->app->baseConfig->getConfig("unixSocket", "database", "/var/run/mysqld/mysqld.sock");
+
+        $connection = mysqli_connect($host, $username, $password, $dbName, $port, $socket);
+        $this->connections[$name] = $connection;
+
+        return $connection->query($query, MYSQLI_ASYNC);
+    }
+
+    public function getData($name, $cacheTime = 3600)
+    {
+        $key = sha1($name);
+
+        if($cacheTime > 0) {
+            $result = !empty($this->app->Cache->get($key)) ? unserialize($this->app->Cache->get($key)) : null;
+            if(!empty($result))
+                return $result;
         }
 
         if(!isset($this->connections[$name]))
@@ -50,6 +66,12 @@ class DbAsync
             mysqli_poll($links, $errors, $reject, $this->timeout);
         } while(!in_array($connection, $links, true) && !in_array($connection, $errors, true) && !in_array($connection, $reject, true));
 
-        return $connection->reap_async_query();
+        $data = $connection->reap_async_query()->fetch_assoc();
+
+        if($cacheTime > 0)
+            $this->app->Cache->set($key, serialize($data), min(3600, $cacheTime));
+
+        return $data;
+
     }
 }
