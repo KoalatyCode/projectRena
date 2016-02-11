821893ba059f5da1ab68901b2c1270535d7bc7ac
Michael Karbowiak <karbowiak@gmail.com>
2016-02-11 01:53:27 +0100

    Add the routes for the individual pages, plus all the controllers for them.. no data yet tho, but it'll come

--- config/routes/killboard.php
+++ config/routes/killboard.php
@@ -1,6 +1,57 @@
 <?php
 $app->group("/kb", function () use ($app) {
+    // Frontpage
     $app->get("/", function() use ($app) {
         (new \ProjectRena\Controller\Killboard\IndexController($app))->index();
+    });
+
+    // Killmail page
+    $app->get("/killmail/:killID/", function() use ($app) {
+
+    });
+
+    // Character page
+    $app->get("/character/:character(/:extraArguments+)/", function($character, $extraArguments = array()) use ($app) {
+
+    });
+
+    // Corporation page
+    $app->get("/corporation/:corporation(/:extraArguments+)/", function($corporation, $extraArguments = array()) use ($app) {
+
+    });
+
+    // Alliance page
+    $app->get("/alliance/:alliance(/:extraArguments+)/", function($alliance, $extraArguments = array()) use ($app) {
+
+    });
+
+    // Faction page
+    $app->get("/faction/:faction(/:extraArguments+)/", function($faction, $extraArguments = array()) use ($app) {
+
+    });
+
+    // System page
+    $app->get("/system/:system(/:extraArguments+)/", function($system, $extraArguments = array()) use ($app) {
+
+    });
+
+    // Region page
+    $app->get("/region/:region(/:extraArguments+)/", function($region, $extraArguments = array()) use ($app) {
+
+    });
+
+    // Ship page
+    $app->get("/ship/:ship(/:extraArguments+)/", function($ship, $extraArguments = array()) use ($app) {
+
+    });
+
+    // Group page
+    $app->get("/group/:group(/:extraArguments+)/", function($group, $extraArguments = array()) use ($app) {
+
+    });
+
+    // Weapon page
+    $app->get("/weapon/:weapon(/:extraArguments+)/", function($weapon, $extraArguments = array()) use ($app) {
+
     });
 });
---
+++ src/Controller/Killboard/CharacterController.php
@@ -0,0 +1,69 @@
+<?php
+namespace ProjectRena\Controller\Killboard;
+
+use ProjectRena\RenaApp;
+
+/**
+ * Functions for the API
+ */
+class CharacterController
+{
+    /**
+     * The Slim Application
+     */
+    private $app;
+
+    /**
+     * The Cache
+     */
+    private $cache;
+
+    /**
+     * The baseConfig (config/config.php)
+     */
+    private $config;
+
+    /**
+     * cURL interface (getData / setData)
+     */
+    private $curl;
+
+    /**
+     * The Database
+     */
+    private $db;
+
+    /**
+     * The logger, outputs to logs/app.log
+     */
+    private $log;
+
+    /**
+     * StatsD for tracking stats
+     */
+    private $statsd;
+
+    /**
+     * @var string
+     */
+    private $contentType;
+
+    /**
+     * @param RenaApp $app
+     */
+    public function __construct(RenaApp $app)
+    {
+        $this->app = $app;
+        $this->db = $app->Db;
+        $this->config = $app->baseConfig;
+        $this->cache = $app->Cache;
+        $this->curl = $app->cURL;
+        $this->statsd = $app->StatsD;
+        $this->log = $app->Logging;
+        $this->contentType = "application/json";
+    }
+
+    public function index()
+    {
+    }
+}
