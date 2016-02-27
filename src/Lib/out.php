94de07512783d1530d91d348ec44e401407e6d65
Michael Karbowiak <karbowiak@gmail.com>
2016-02-27 16:45:04 +0100

    Move the extra info from everything to twig again

--- src/Lib/out.php
+++ src/Lib/out.php
@@ -41,41 +41,6 @@ class out
         if ($contentType)
             $this->contentType = $contentType;
 
-        // Generate character Information array data
-        $characterInformation = array();
-        if (isset($_SESSION["characterID"])) {
-            $characterInformation = $this->app->characters->getAllByID($_SESSION["characterID"]);
-            $characterInformation["history"] = json_decode($characterInformation["history"], true);
-            $characterInformation["corporation"] = $this->app->corporations->getAllByID($this->app->characters->getAllByID($_SESSION["characterID"])["corporationID"]);
-            $characterInformation["corporation"]["information"] = json_decode($characterInformation["corporation"]["information"], true);
-            $characterInformation["alliance"] = $this->app->alliances->getAllByID($this->app->characters->getAllByID($_SESSION["characterID"])["allianceID"]);
-            $characterInformation["groups"] = $this->app->UsersGroups->getGroup($this->app->Users->getUserByName($characterInformation["characterName"])["id"]);
-        }
-
-        $extraData = array(
-            "siteData" => array(
-                "EVESSOURL" => $this->app->EVEOAuth->LoginURL(),
-                "imageServer" => $this->app->baseConfig->getConfig("imageServer", "ccp"),
-                "apiServer" => $this->app->baseConfig->getConfig("apiServer", "ccp"),
-                "asyncQueryCount" => $this->app->DbAsync->getQueryCount(),
-                "asyncQueryTime" => $this->app->DbAsync->getQueryTime(),
-                "syncQueryCount" => $this->app->Db->getQueryCount(),
-                "syncQueryTime" => $this->app->Db->getQueryTime(),
-                "totalQueryCount" => $this->app->DbAsync->getQueryCount() + $this->app->Db->getQueryCount(),
-                "totalQueryTime" => $this->app->DbAsync->getQueryTime() + $this->app->Db->getQueryTime(),
-                "siteLoadTime" => $this->app->timer->stop(),
-                "phpTime" => $this->app->timer->stop() - ($this->app->DbAsync->getQueryTime() + $this->app->Db->getQueryTime())
-            ),
-            "userData" => array(
-                "loggedIn" => isset($_SESSION["loggedIn"]) ? true : false,
-                "characterName" => isset($_SESSION["characterName"]) ? $_SESSION["characterName"] : null,
-                "characterID" => isset($_SESSION["characterID"]) ? $_SESSION["characterID"] : null,
-                "charInfo" => $characterInformation,
-            )
-        );
-
-        $dataArray = array_merge($extraData, array("pageData" => $dataArray));
-
         if ($this->contentType == "application/json")
             return $this->toJson($dataArray, $status);
         if ($this->contentType == "application/xml")
@@ -118,6 +83,41 @@ class out
      */
     public function toTwig($templateFile, $dataArray = array(), $status = null)
     {
+        // Generate character Information array data
+        $characterInformation = array();
+        if (isset($_SESSION["characterID"])) {
+            $characterInformation = $this->app->characters->getAllByID($_SESSION["characterID"]);
+            $characterInformation["history"] = json_decode($characterInformation["history"], true);
+            $characterInformation["corporation"] = $this->app->corporations->getAllByID($this->app->characters->getAllByID($_SESSION["characterID"])["corporationID"]);
+            $characterInformation["corporation"]["information"] = json_decode($characterInformation["corporation"]["information"], true);
+            $characterInformation["alliance"] = $this->app->alliances->getAllByID($this->app->characters->getAllByID($_SESSION["characterID"])["allianceID"]);
+            $characterInformation["groups"] = $this->app->UsersGroups->getGroup($this->app->Users->getUserByName($characterInformation["characterName"])["id"]);
+        }
+
+        $extraData = array(
+            "siteData" => array(
+                "EVESSOURL" => $this->app->EVEOAuth->LoginURL(),
+                "imageServer" => $this->app->baseConfig->getConfig("imageServer", "ccp"),
+                "apiServer" => $this->app->baseConfig->getConfig("apiServer", "ccp"),
+                "asyncQueryCount" => $this->app->DbAsync->getQueryCount(),
+                "asyncQueryTime" => $this->app->DbAsync->getQueryTime(),
+                "syncQueryCount" => $this->app->Db->getQueryCount(),
+                "syncQueryTime" => $this->app->Db->getQueryTime(),
+                "totalQueryCount" => $this->app->DbAsync->getQueryCount() + $this->app->Db->getQueryCount(),
+                "totalQueryTime" => $this->app->DbAsync->getQueryTime() + $this->app->Db->getQueryTime(),
+                "siteLoadTime" => $this->app->timer->stop(),
+                "phpTime" => $this->app->timer->stop() - ($this->app->DbAsync->getQueryTime() + $this->app->Db->getQueryTime())
+            ),
+            "userData" => array(
+                "loggedIn" => isset($_SESSION["loggedIn"]) ? true : false,
+                "characterName" => isset($_SESSION["characterName"]) ? $_SESSION["characterName"] : null,
+                "characterID" => isset($_SESSION["characterID"]) ? $_SESSION["characterID"] : null,
+                "charInfo" => $characterInformation,
+            )
+        );
+
+        $dataArray = array_merge($extraData, array("pageData" => $dataArray));
+
         $this->app->render($templateFile, $dataArray, $status);
     }
 
