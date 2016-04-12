4d924d977cc043fda38127bf4e039e29b2fcfe24
Michael Karbowiak <karbowiak@gmail.com>
2016-04-12 03:46:50 +0200

    Victims, we need victims..

--- src/Controller/API/KilllistAPIController.php
+++ src/Controller/API/KilllistAPIController.php
@@ -77,7 +77,7 @@ class KilllistAPIController
     
     public function bigKills()
     {
-        $kills = $this->db->query("SELECT killID FROM participants WHERE groupID IN (547,485,513,902,941,30, 659) GROUP BY killID ORDER BY killTime DESC LIMIT 100", array(), 60);
+        $kills = $this->db->query("SELECT killID FROM participants WHERE groupID IN (547,485,513,902,941,30, 659) AND isVictim = 1 GROUP BY killID ORDER BY killTime DESC LIMIT 100", array(), 60);
         $killIDs = array();
         foreach($kills as $kid)
             $killIDs[] = $kid["killID"];
@@ -88,7 +88,7 @@ class KilllistAPIController
     
     public function wSpace()
     {
-        $kills = $this->db->query("SELECT killID FROM participants WHERE (regionID >= 11000001 and regionID <= 11000033) GROUP BY killID ORDER BY killTime DESC LIMIT 100", array(), 60);
+        $kills = $this->db->query("SELECT killID FROM participants WHERE (regionID >= 11000001 and regionID <= 11000033) AND isVictim = 1 GROUP BY killID ORDER BY killTime DESC LIMIT 100", array(), 60);
         $killIDs = array();
         foreach($kills as $kid)
             $killIDs[] = $kid["killID"];
@@ -99,7 +99,7 @@ class KilllistAPIController
     
     public function highSec()
     {
-        $kills = $this->db->query("SELECT killID FROM participants WHERE solarSystemID IN (SELECT solarSystemID FROM mapSolarSystems WHERE security > 0.45 GROUP BY solarSystemID) GROUP BY killID ORDER BY killTime DESC LIMIT 100", array(), 60);
+        $kills = $this->db->query("SELECT killID FROM participants WHERE solarSystemID IN (SELECT solarSystemID FROM mapSolarSystems WHERE security > 0.45 GROUP BY solarSystemID) AND isVictim = 1 GROUP BY killID ORDER BY killTime DESC LIMIT 100", array(), 60);
         $killIDs = array();
         foreach($kills as $kid)
             $killIDs[] = $kid["killID"];
@@ -110,7 +110,7 @@ class KilllistAPIController
 
     public function lowSec()
     {
-        $kills = $this->db->query("SELECT killID FROM participants WHERE solarSystemID IN (SELECT solarSystemID FROM mapSolarSystems WHERE security <= 0.45 GROUP BY solarSystemID) GROUP BY killID ORDER BY killTime DESC LIMIT 100", array(), 60);
+        $kills = $this->db->query("SELECT killID FROM participants WHERE solarSystemID IN (SELECT solarSystemID FROM mapSolarSystems WHERE security <= 0.45 GROUP BY solarSystemID) AND isVictim = 1 GROUP BY killID ORDER BY killTime DESC LIMIT 100", array(), 60);
         $killIDs = array();
         foreach($kills as $kid)
             $killIDs[] = $kid["killID"];
@@ -121,7 +121,7 @@ class KilllistAPIController
     
     public function nullSec()
     {
-        $kills = $this->db->query("SELECT killID FROM participants WHERE solarSystemID IN (SELECT solarSystemID FROM mapSolarSystems WHERE security <= 0 AND (regionID < 11000001 or regionID > 11000030) GROUP BY solarSystemID) GROUP BY killID ORDER BY killTime DESC LIMIT 100", array(), 60);
+        $kills = $this->db->query("SELECT killID FROM participants WHERE solarSystemID IN (SELECT solarSystemID FROM mapSolarSystems WHERE security <= 0 AND (regionID < 11000001 or regionID > 11000030) GROUP BY solarSystemID) AND isVictim = 1 GROUP BY killID ORDER BY killTime DESC LIMIT 100", array(), 60);
         $killIDs = array();
         foreach($kills as $kid)
             $killIDs[] = $kid["killID"];
@@ -132,7 +132,7 @@ class KilllistAPIController
 
     public function solo()
     {
-        $kills = $this->db->query("SELECT killID FROM participants WHERE numberInvolved = 1 AND vGroupID NOT IN (670, 237, 29, 31) GROUP BY killID ORDER BY killTime DESC LIMIT 100", array(), 60);
+        $kills = $this->db->query("SELECT killID FROM participants WHERE numberInvolved = 1 AND vGroupID NOT IN (670, 237, 29, 31) AND isVictim = 1 GROUP BY killID ORDER BY killTime DESC LIMIT 100", array(), 60);
         $killIDs = array();
         foreach($kills as $kid)
             $killIDs[] = $kid["killID"];
@@ -143,7 +143,7 @@ class KilllistAPIController
 
     public function fiveB()
     {
-        $kills = $this->db->query("SELECT killID FROM participants WHERE totalValue >= 5000000000 GROUP BY killID ORDER BY killTime DESC LIMIT 100", array(), 60);
+        $kills = $this->db->query("SELECT killID FROM participants WHERE totalValue >= 5000000000 AND isVictim = 1 GROUP BY killID ORDER BY killTime DESC LIMIT 100", array(), 60);
         $killIDs = array();
         foreach($kills as $kid)
             $killIDs[] = $kid["killID"];
@@ -154,7 +154,7 @@ class KilllistAPIController
     
     public function tenB()
     {
-        $kills = $this->db->query("SELECT killID FROM participants WHERE totalValue >= 10000000000 GROUP BY killID ORDER BY killTime DESC LIMIT 100", array(), 60);
+        $kills = $this->db->query("SELECT killID FROM participants WHERE totalValue >= 10000000000 AND isVictim = 1 GROUP BY killID ORDER BY killTime DESC LIMIT 100", array(), 60);
         $killIDs = array();
         foreach($kills as $kid)
             $killIDs[] = $kid["killID"];
@@ -165,7 +165,7 @@ class KilllistAPIController
 
     public function capitals()
     {
-        $kills = $this->db->query("SELECT killID FROM participants WHERE groupID IN (547, 485) GROUP BY killID ORDER BY killTime DESC LIMIT 100", array(), 60);
+        $kills = $this->db->query("SELECT killID FROM participants WHERE groupID IN (547, 485) AND isVictim = 1 GROUP BY killID ORDER BY killTime DESC LIMIT 100", array(), 60);
         $killIDs = array();
         foreach($kills as $kid)
             $killIDs[] = $kid["killID"];
@@ -176,7 +176,7 @@ class KilllistAPIController
 
     public function superCarriers()
     {
-        $kills = $this->db->query("SELECT killID FROM participants WHERE groupID = 659 GROUP BY killID ORDER BY killTime DESC LIMIT 100", array(), 60);
+        $kills = $this->db->query("SELECT killID FROM participants WHERE groupID = 659 AND isVictim = 1 GROUP BY killID ORDER BY killTime DESC LIMIT 100", array(), 60);
         $killIDs = array();
         foreach($kills as $kid)
             $killIDs[] = $kid["killID"];
@@ -187,7 +187,7 @@ class KilllistAPIController
 
     public function titans()
     {
-        $kills = $this->db->query("SELECT killID FROM participants WHERE groupID = 30 GROUP BY killID ORDER BY killTime DESC LIMIT 100", array(), 60);
+        $kills = $this->db->query("SELECT killID FROM participants WHERE groupID = 30 AND isVictim = 1 GROUP BY killID ORDER BY killTime DESC LIMIT 100", array(), 60);
         $killIDs = array();
         foreach($kills as $kid)
             $killIDs[] = $kid["killID"];
@@ -198,7 +198,7 @@ class KilllistAPIController
 
     public function freighters()
     {
-        $kills = $this->db->query("SELECT killID FROM participants WHERE groupID IN (513, 902, 941) GROUP BY killID ORDER BY killTime DESC LIMIT 100", array(), 60);
+        $kills = $this->db->query("SELECT killID FROM participants WHERE groupID IN (513, 902, 941) AND isVictim = 1 GROUP BY killID ORDER BY killTime DESC LIMIT 100", array(), 60);
         $killIDs = array();
         foreach($kills as $kid)
             $killIDs[] = $kid["killID"];
@@ -209,7 +209,7 @@ class KilllistAPIController
 
     public function t1()
     {
-        $kills = $this->db->query("SELECT killID FROM participants WHERE groupID IN (419,27,29,547,26,420,25,28,941,463,237,31) GROUP BY killID ORDER BY killTime DESC LIMIT 100", array(), 60);
+        $kills = $this->db->query("SELECT killID FROM participants WHERE groupID IN (419,27,29,547,26,420,25,28,941,463,237,31) AND isVictim = 1 GROUP BY killID ORDER BY killTime DESC LIMIT 100", array(), 60);
         $killIDs = array();
         foreach($kills as $kid)
             $killIDs[] = $kid["killID"];
@@ -220,7 +220,7 @@ class KilllistAPIController
 
     public function t2()
     {
-        $kills = $this->db->query("SELECT killID FROM participants WHERE groupID IN (324,898,906,540,830,893,543,541,833,358,894,831,902,832,900,834,380) GROUP BY killID ORDER BY killTime DESC LIMIT 100", array(), 60);
+        $kills = $this->db->query("SELECT killID FROM participants WHERE groupID IN (324,898,906,540,830,893,543,541,833,358,894,831,902,832,900,834,380) AND isVictim = 1 GROUP BY killID ORDER BY killTime DESC LIMIT 100", array(), 60);
         $killIDs = array();
         foreach($kills as $kid)
             $killIDs[] = $kid["killID"];
@@ -231,7 +231,7 @@ class KilllistAPIController
 
     public function t3()
     {
-        $kills = $this->db->query("SELECT killID FROM participants WHERE groupID IN (963) GROUP BY killID ORDER BY killTime DESC LIMIT 100", array(), 60);
+        $kills = $this->db->query("SELECT killID FROM participants WHERE groupID IN (963) AND isVictim = 1 GROUP BY killID ORDER BY killTime DESC LIMIT 100", array(), 60);
         $killIDs = array();
         foreach($kills as $kid)
             $killIDs[] = $kid["killID"];
@@ -242,7 +242,7 @@ class KilllistAPIController
 
     public function frigates()
     {
-        $kills = $this->db->query("SELECT killID FROM participants WHERE groupID IN (324,893,25,831,237) GROUP BY killID ORDER BY killTime DESC LIMIT 100", array(), 60);
+        $kills = $this->db->query("SELECT killID FROM participants WHERE groupID IN (324,893,25,831,237) AND isVictim = 1 GROUP BY killID ORDER BY killTime DESC LIMIT 100", array(), 60);
         $killIDs = array();
         foreach($kills as $kid)
             $killIDs[] = $kid["killID"];
@@ -253,7 +253,7 @@ class KilllistAPIController
 
     public function destroyers()
     {
-        $kills = $this->db->query("SELECT killID FROM participants WHERE groupID IN (420,541) GROUP BY killID ORDER BY killTime DESC LIMIT 100", array(), 60);
+        $kills = $this->db->query("SELECT killID FROM participants WHERE groupID IN (420,541) AND isVictim = 1 GROUP BY killID ORDER BY killTime DESC LIMIT 100", array(), 60);
         $killIDs = array();
         foreach($kills as $kid)
             $killIDs[] = $kid["killID"];
@@ -264,7 +264,7 @@ class KilllistAPIController
 
     public function cruisers()
     {
-        $kills = $this->db->query("SELECT killID FROM participants WHERE groupID IN (906,26,833,358,894,832,963) GROUP BY killID ORDER BY killTime DESC LIMIT 100", array(), 60);
+        $kills = $this->db->query("SELECT killID FROM participants WHERE groupID IN (906,26,833,358,894,832,963) AND isVictim = 1 GROUP BY killID ORDER BY killTime DESC LIMIT 100", array(), 60);
         $killIDs = array();
         foreach($kills as $kid)
             $killIDs[] = $kid["killID"];
@@ -275,7 +275,7 @@ class KilllistAPIController
 
     public function battlecruisers()
     {
-        $kills = $this->db->query("SELECT killID FROM participants WHERE groupID IN (419,540) GROUP BY killID ORDER BY killTime DESC LIMIT 100", array(), 60);
+        $kills = $this->db->query("SELECT killID FROM participants WHERE groupID IN (419,540) AND isVictim = 1 GROUP BY killID ORDER BY killTime DESC LIMIT 100", array(), 60);
         $killIDs = array();
         foreach($kills as $kid)
             $killIDs[] = $kid["killID"];
@@ -286,7 +286,7 @@ class KilllistAPIController
 
     public function battleships()
     {
-        $kills = $this->db->query("SELECT killID FROM participants WHERE groupID IN (27,898,900) GROUP BY killID ORDER BY killTime DESC LIMIT 100", array(), 60);
+        $kills = $this->db->query("SELECT killID FROM participants WHERE groupID IN (27,898,900) AND isVictim = 1 GROUP BY killID ORDER BY killTime DESC LIMIT 100", array(), 60);
         $killIDs = array();
         foreach($kills as $kid)
             $killIDs[] = $kid["killID"];
