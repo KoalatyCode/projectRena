d0064478a9711883020a26d4e3d8fb1057efde65
Michael Karbowiak <karbowiak@gmail.com>
2016-04-18 17:38:34 +0200

    Update apidocs

--- public/docs/api_project.js
+++ public/docs/api_project.js
@@ -17,7 +17,7 @@ define({
   "apidoc": "0.2.0",
   "generator": {
     "name": "apidoc",
-    "time": "2016-04-14T00:31:55.079Z",
+    "time": "2016-04-18T15:38:25.942Z",
     "url": "http://apidocjs.com",
     "version": "0.15.1"
   }
